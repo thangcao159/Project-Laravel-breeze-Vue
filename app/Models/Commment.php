d10667f4228fb920577bd7a2d2baef9877692b05
thangcao159 <thangcao159@gmail.com>
2024-07-24 17:32:22 +0700

    Project Laravel/breeze + Vue

---
+++ app/Models/Commment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Commment extends Model
+{
+    use HasFactory;
+}
